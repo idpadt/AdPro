bd451a4dac595047243da623cedfbc70eec53862
Aditya <idpadityarahman@gmail.com>
2025-02-14 13:59:07 +0700

    added homepage & func tests

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String createHomePage() {
+        return "homePage";
+    }
+}
