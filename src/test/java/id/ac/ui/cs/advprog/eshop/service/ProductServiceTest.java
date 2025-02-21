e7b99f2453a134bd42d981c51bd3cfd4d59be932
Aditya <idpadityarahman@gmail.com>
2025-02-21 18:29:55 +0700

    fix code issue : remove public modifier

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceTest.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ProductServiceTest {
+class ProductServiceTest {
 
     @Mock
     private ProductRepository productRepository;
